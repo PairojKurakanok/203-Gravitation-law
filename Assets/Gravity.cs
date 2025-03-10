7b7f90b91c74c29133ca5a9de649bf4c87e4fadd
PairojKurakanok <pairoj.kura@bumail.net>
2025-03-10 16:31:49 +0700

    ใส่สคริปเสร้จแล้ว

--- Assets/Gravity.cs
+++ Assets/Gravity.cs
@@ -1,13 +1,32 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Gravity : MonoBehaviour
 {
-    private Rigidbody rb;
-
+    Rigidbody rb;
+    const float G = 0.006674f;
+    public static List<Gravity> planetlist;
+    
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if (planetlist == null)
+        {
+            planetlist = new List<Gravity>();
+        }
+        
+        planetlist.Add(this);
+    }
+    
+    private void FixedUpdate()
+    {
+        //call Attract
+        foreach (var planet in planetlist)
+        {
+            if (planet != this)
+                Attract(planet);
+        }
     }
 
     void Attract(Gravity other)
@@ -18,6 +37,9 @@ public class Gravity : MonoBehaviour
         //get distance in meter    
         float distance = direction.magnitude;
         //calculate Gravity force
-        float forceMagnitude =
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+        
+        otherRb.AddForce(finalForce);
     }
 }
